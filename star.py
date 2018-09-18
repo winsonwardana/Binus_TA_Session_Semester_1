6f3abb968008117ca6d73e19ed75d98a5a7e69d5
winson <winson91100@gmail.com>
2018-09-18 14:38:36 +0700

    first commit

---
+++ star.py
@@ -0,0 +1,15 @@
+#star=int(input("input number:"))
+#x=1
+#while x<star+1 :
+    #print("*")
+    #x=x+1
+    #while x<star+1:
+        #print("*" * x)
+       # x=x+1
+x=0
+y=int(input("input a number:"))
+while x < y :
+     y -= 1
+     print("*" * (y+1))
+     if x == y:
+         break
