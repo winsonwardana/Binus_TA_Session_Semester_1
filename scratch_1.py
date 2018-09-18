e89510386f03d149b8041e50825865b9f2b1cc15
winson <winson91100@gmail.com>
2018-09-18 14:48:45 +0700

    first commit

---
+++ scratch_1.py
@@ -0,0 +1,24 @@
+#star =int(input("Input number:"))
+#for x in range (star):
+    #print("*" *(star))
+   # star = star*4
+#star =int(input("Input number:"))
+#for x in range (star+1):
+    #print("*" * x)
+
+#i=int(input("input Number:"))
+#for i in range(1,i+1):
+   # print("*"* i)
+
+#number=int(input("input number:"))
+#for i in range(1,number+1):
+    #print("*" *(number-i+1))
+
+
+
+
+
+
+
+
+
